7a844fa401306a2d6852c23b13260bc799add45b
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2025-07-03 14:02:39 +0530

    Update arraysSlice.go

--- golang-practicle-practice/arraySlice/arraysSlice.go
+++ golang-practicle-practice/arraySlice/arraysSlice.go
@@ -16,4 +16,41 @@ func main() {
 	for i, x := range array {
 		fmt.Println("value at index ", i, "is :", x)
 	}
+
+
+
+	var num []int		//a slice of unspecified size.. similar to num == []int {0,0,0,0,0}
+	
+	num1 :=make([]int, 4, 7)
+	//if a slice is declared with no inputs, then by default, it is initialized as nil, its length and capacity are zero.
+
+	if num == nil{
+		fmt.Println("slice is nil")
+	}
+
+
+	//1. Slice declaration and initialization with values:
+	s1 := []int{1,2,3,4,5}
+
+	//2. Declare without initializing (nil slice)
+	var s2 []int
+
+	//using make
+	s3 := make([]int, 3, 4)		//len 3 and cap 4
+	s4 := make([]int 3)		//len 3 and cap 3
+
+
+	//apend
+	var s5 []int
+	s5=append(s5, 10,20, 30)
+
+
+	//slice from an array:
+	arr := [5]int{1,2,3,4,5}
+	s6 := arr[1:4]	//2,3,4
+
+
+	s7 := arr[1:]	//2,3,4,5
+
+	
 }
