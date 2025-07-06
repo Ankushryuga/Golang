37e96d7922b9c0f34323b1decf4118a76d1ce043
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2025-07-06 13:11:55 +0530

    Update and rename 1-2-assigning-function-to-variable.go to 1-1-Function-as-value.go

--- golang-practicle-practice/go-functions/1-1-Function-as-value.go
+++ golang-practicle-practice/go-functions/1-1-Function-as-value.go
@@ -1,7 +1,9 @@
 /**
 Go allows you to assign a function directly to a variable. The function should have a return type.
 */
-
+/**
+go have flexibility to create functions on fly and use them as value...
+*/
 package main
 import "fmt"
 
@@ -13,4 +15,11 @@ func main(){
   sum := addTwoNumbers
   result := sum(19,30)
   fmt.Println("result: ", result) 
+
+
+   getSquareRoot := func(x float64) float64{
+    return math.sqrt(x)
+  }
+  //use the functions::
+  fmt.Println(getSquareRoot(9))
 }
