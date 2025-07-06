327ee26f6aa4a17549950828cefb5490a11b922f
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2025-07-06 14:44:25 +0530

    Create 1-4-go-anonymous-function.go

---
+++ golang-practicle-practice/go-functions/1-4-go-anonymous-function.go
@@ -0,0 +1,70 @@
+/***
+Anonymous function also known as lambda function or function literals, are defined without a name..
+Go Anonymous functions allow you to define temporary, one-time use functions exactly where you want them, without giving them a name.
+
+## Ideal for:
+1. Executing code on the spot.
+2. Satisfying variable usage from surrounding code.
+3. Scheduling fast jobs in goroutine
+*/
+
+
+package main
+
+import "fmt"
+
+/**
+Anonymous function can access variables from their enclosing scope, creating closure...
+*/
+func counter() func() int{
+  count := 0
+  return func() int{
+    count++
+    return count
+  }
+}
+
+func multiplier(factor int) func(int) int{
+  return func(val int) int{
+    return val * factor
+  }
+}
+
+func main(){
+  greet := func(){
+    fmt.Println("Tutorials point")
+  }
+  greet()  //call the anonymous function.
+  
+  add := func(a, b int) int{
+    return a+b
+  }
+
+  //Invoke function expression::
+  //without parameter:
+  func (){
+    fmt.Println("Executing immediately")
+  }()
+
+  //with parameters:
+  result := func(a, b int) int{
+    return a * b
+  }(50, 10)
+
+  fmt.Println(result)
+  
+  sum := add(34, 55)
+  fmt.Println("sum: ", sum)
+
+
+  c := counter()
+  fmt.Println(c())    //1
+  fmt.Println(c())  //2
+  fmt.Println(c())  //3
+
+  double := multiplier(2)
+  tripler := multiplier(3)
+
+  fmt.Println(double(6))  //2*6=12
+  fmt.Println(tripler(4))  //3*4=12
+}
