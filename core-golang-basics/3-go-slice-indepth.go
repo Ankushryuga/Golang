c8d902ee8c81931a2c7ff4ead043e24be59096f7
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2025-07-16 15:42:30 +0530

    Update 3-go-slice-indepth.go

--- core-golang-basics/3-go-slice-indepth.go
+++ core-golang-basics/3-go-slice-indepth.go
@@ -2,6 +2,13 @@
 - In Go slices are designed to hold elements of a single, specific type. This means you cannot directly create a slice that contains elements of different types.
 # However, there are workarounds to achieve similar functionality.
 
+# An uninitialized slice equals to nil and has length 0.
+# To create a slice with non-zero length, use the buildin make.
+# By default a new slice's capacity is equal to its length;
+
+
+# Slice support a "slice" operator with the syntax slice[low:high].
+
 - 1. Using interface{}
 - 2. Using struct{}
 - 3. Map
@@ -13,6 +20,47 @@
 
 package main
 import "fmt"
+
+//# Slice support a "slice" operator with the syntax slice[low:high].
+func sliceOperators(){
+  var s[]string
+  fmt.Println("uninit: ", s, s==nil, len(s)==0)
+
+  s = make([]string, 3)
+  fmt.Println("s: ", s, "len:", len(s), "caps:" cap(s))
+
+  s[0]="a"
+  s[1]="b"
+  s[2]="c"
+
+  fmt.Println("set: ", s)
+  s.append(s, "d")
+  s.append(s, "e")
+
+  c := make([]string, len(s))
+  copy(c, s)
+  fmt.Println("copy:", c)
+
+  l:=s[2:5]  //low:2, and high:5
+  fmt.Println("slice1", l)
+
+  l=s[:5]  //slices up to (but excluding)
+  fmt.Println("slice2", l)
+
+  l =s[2:]  //slice up from (and including)
+  fmt.Println("slice3", l)
+
+  twoD := make([][]int 3)
+  for i:=0;i<3;i++{
+    innerLen := i+1
+    twoD[i]=make([]int, innerLen)
+    for j:=0;j<innerLen;j++{
+      twoD[i][j]=i+j  
+    }
+  }
+  fmt.Println("2d", twoD)
+}
+
 
 //1-Using interfaces
 func interfaceSlice(){
