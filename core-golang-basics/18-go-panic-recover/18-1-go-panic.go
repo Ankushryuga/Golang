6a323ea3e905bf0ae5fad18fe5a01eaafa9b7c60
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2025-07-20 12:39:10 +0530

    Update 18-1-go-panic.go

--- core-golang-basics/18-go-panic-recover/18-1-go-panic.go
+++ core-golang-basics/18-go-panic-recover/18-1-go-panic.go
@@ -1,4 +1,28 @@
 /****
 panic is Go's built-in mechanism for handling unexpected runtime errors. when a panic occurs, it immediately stops execution of the current function and begins 
 unwinding the stack, running any defer statements along the way. If the panic reaches the top of goroutine and isn't recovered, the program crashes.
+
+## rules of thumb.
+| Situation                    | Use `panic`?       |
+| ---------------------------- | ------------------ |
+| Logic bug / impossible state | ✅ Yes              |
+| Input error or user behavior | ❌ No (use `error`) |
+| Library misuse by caller     | ✅ Maybe            |
+| General flow control         | ❌ Never            |
+
 */
+
+package main
+import "fmt"
+
+func main(){
+  //Recovering from panic
+  defer func(){
+    if r:=recover(); r!=nil{
+      fmt.Println("Recovered from panic", r)
+    }
+  }()
+  fmt.Println("About to panic")
+  panic("crash")
+  fmt.Println("This will not run")
+}
