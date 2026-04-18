625ec3b849e28bed7ae801d05122647a3a04c7fa
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2026-04-18 23:32:00 +0530

    Update main.go

--- Go_Backend/Go_Advance_Concept/Interfaces/Go_Methods/main.go
+++ Go_Backend/Go_Advance_Concept/Interfaces/Go_Methods/main.go
@@ -1,1 +1,18 @@
 // Go Methods examples
+/**
+Go methods are functions that is associated to a type.
+**/
+
+package main
+
+import "fmt"
+
+type Employee struct{
+  ID    int
+  Name  string
+  Desg  string
+}
+
+// Get employee information and error info
+func (e *Employee) GetEmployee(ID  int) Employee, error {
+}
